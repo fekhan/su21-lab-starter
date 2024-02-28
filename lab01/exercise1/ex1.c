ed99f2a36d89c314d55b78109312f256efc759dc
fekhan <ferman_khan@hotmail.com>
2024-02-28 16:13:59 +0500

    Added missing/desired code and test case to lab01 exercise 1 tasks.

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -7,7 +7,25 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    return 0;
+    int count = 0;
+
+    // Method 1: Using strlen()
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] == letter) {
+            count++;
+        }
+    }
+
+    // Method 2: Using null terminator
+/*    char *ptr = str;
+    while (*ptr != '\0') {
+        if (*ptr == letter) {
+            count++;
+        }
+        ptr++;
+    }
+*/
+    return count;
 }
 
 /* Populates DNA_SEQ with the number of times each nucleotide appears.
@@ -15,5 +33,18 @@ Each sequence will end with a NULL terminator and will have up to 20 nucleotides
 All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
+    dna_seq->A_count = 0;
+    dna_seq->C_count = 0;
+    dna_seq->G_count = 0;
+    dna_seq->T_count = 0;
+
+    for (int i = 0; i < strlen(dna_seq->sequence); i++) {
+        switch (dna_seq->sequence[i]) {
+            case 'A': dna_seq->A_count++; break;
+            case 'C': dna_seq->C_count++; break;
+            case 'G': dna_seq->G_count++; break;
+            case 'T': dna_seq->T_count++; break;
+        }
+    }
     return;
 }
