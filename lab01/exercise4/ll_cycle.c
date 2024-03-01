9c9c62662d73de932d34ddaed1610654be595c87
fekhan <ferman_khan@hotmail.com>
2024-03-02 03:51:06 +0500

    Updated ll_cycle.c

--- lab01/exercise4/ll_cycle.c
+++ lab01/exercise4/ll_cycle.c
@@ -1,6 +1,19 @@
 #include <stddef.h>
 #include "ll_cycle.h"
 
+/* 	return 1 if linked list has circle
+	return 0 if don't
+*/
+
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node *fastPtr = head;
+    node *slowPtr = head;
+    while(fastPtr != NULL && fastPtr->next != NULL){
+    fastPtr = fastPtr->next->next;
+    slowPtr = slowPtr->next;
+    if(fastPtr == slowPtr){
+    	return 1;
+    	}
+    }
+    return 0;
 }
